8b5565071abe4136586f20ae162086717796e8a6
Balvant4 <balvantkushwaha44@gmail.com>
2024-06-18 20:44:50 +0530

    Utils code discussion

--- src/index.js
+++ src/index.js
@@ -7,12 +7,23 @@ import dotenv from "dotenv";
 // import express from "express";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection Faild", err);
+  });
+
+//second way to connect atlas
 
 /*
 (async () => {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+//it simply make method and export for uses
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// Using try catch
+/*
+const asyncHandler = (fn) => async (req,res,next) => {
+  try {
+  } catch (error) {
+    res.status(err.code || 500).json({
+      succes: false,
+      message: err.message,
+    });
+  }
+};
+*/
